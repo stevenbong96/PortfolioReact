960df559b5f3d99c8e15d0e5e719c3250055b4ab
Steven Bong <stevenseal99@yahoo.com>
2020-11-09 22:38:58 -0800

    Added card boiler plate to HW and Project page

--- src/pages/Homework/Homework.js
+++ src/pages/Homework/Homework.js
@@ -1,10 +1,129 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../../utils/API";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
-function Homework(){
-    return(
-        <div>
-            <h1>This is Homework Page!</h1>
-        </div>
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+        marginTop: "3%"
+    },
+    media: {
+        height: 140,
+    },
+});
+
+function Homework() {
+    const classes = useStyles();
+
+    // const [githubInfo, setGithubInfo] = useState({
+    //     homework: "",
+    // })
+
+    // useEffect(() => {
+    //     loadGithubInfo();
+    // }, [])
+
+    // function loadGithubInfo () {
+    //     API.getGithub()
+    //     .then(res => {
+    //         console.log(res);
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     })
+    // }
+
+    return (
+        <Grid container direction="row" justify="space-around" alignItems="center">
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Lizard
+                    </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Github
+                </Button>
+                    <Button size="small" color="primary">
+                        Deployed Website
+                </Button>
+                </CardActions>
+            </Card>
+
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Lizard
+                    </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Github
+                </Button>
+                    <Button size="small" color="primary">
+                        Deployed Website
+                </Button>
+                </CardActions>
+            </Card>
+
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Lizard
+                    </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Github
+                </Button>
+                    <Button size="small" color="primary">
+                        Deployed Website
+                </Button>
+                </CardActions>
+            </Card>
+        </Grid>
     )
 }
 
--- src/pages/Project/Project.js
+++ src/pages/Project/Project.js
@@ -1,11 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import API from "../../utils/API";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
-function Project(){
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+        marginTop: "3%"
+    },
+    media: {
+        height: 140,
+    },
+});
+
+function Project() {
+    const classes = useStyles();
+
     // const [githubInfo, setGithubInfo] = useState({
     //     project: "",
-    //     homework: "",
-    //     portfolio: ""
     // })
 
     // useEffect(() => {
@@ -22,10 +41,89 @@ function Project(){
     //     })
     // }
 
-    return(
-        <div>
-            <h1>This is Project Page!</h1>
-        </div>
+    return (
+        <Grid container direction="row" justify="space-around" alignItems="center">
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Lizard
+                    </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Github
+                </Button>
+                    <Button size="small" color="primary">
+                        Deployed Website
+                </Button>
+                </CardActions>
+            </Card>
+
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Lizard
+                    </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Github
+                </Button>
+                    <Button size="small" color="primary">
+                        Deployed Website
+                </Button>
+                </CardActions>
+            </Card>
+
+            <Card className={classes.root}>
+                <CardActionArea>
+                    <CardMedia
+                        className={classes.media}
+                        image="/static/images/cards/contemplative-reptile.jpg"
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Lizard
+                    </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                            across all continents except Antarctica
+                    </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Github
+                </Button>
+                    <Button size="small" color="primary">
+                        Deployed Website
+                </Button>
+                </CardActions>
+            </Card>
+        </Grid>
     )
 }
 
