7b43f0a830382c76d5da9128b95f9973bd682c8c
Steven Bong <stevenseal99@yahoo.com>
2022-03-30 15:59:47 +0700

    Nevermind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.scss';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Project from "./pages/Project/Project";
+import Contact from "./pages/Contact//Contact";
+import Footer from "./components/Footer/Footer";
+import Resume from "./pages/Resume/Resume";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route exact path="/about"><About /></Route>
+        <Route exact path="/project"><Project /></Route>
+        {/* <Route exact path="/resume"><Resume /></Route> */}
+        <Route exact path="/contact"><Contact /></Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
